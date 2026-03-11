0839e17fd3fb496f8c0780704f350d5f90b9341e
jeni1402 <paul.jenifer@datasirpi.com>
2026-03-11 12:20:18 +0530

    variables in javascript

---
+++ variable.js
@@ -0,0 +1,19 @@
+let a=10;
+const b=2;
+const a1=22;
+
+let c=30;
+ a=23;
+ console.log(a)
+
+
+ const a2=10,a3=20,b3=45;
+ console.log(a2,a3)
+
+ const arr=[0,1,2,3]
+ arr[4]=4;
+ console.log(arr);
+ console.log(arr[1])
+ let d=[1,22.33,4,5]
+ d=[34,55]
+ console.log(d)
