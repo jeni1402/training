9dd8f10132884169cf6b7b8827d14e19e4144be0
jeni1402 <paul.jenifer@datasirpi.com>
2026-03-11 12:21:35 +0530

    datatypes in javascript

---
+++ datatype.js
@@ -0,0 +1,29 @@
+
+function details(){
+// string
+const name="jeni";    
+// number
+const age=23;
+// boolean
+const married=false;
+// big int
+const phno=4721764264n;
+// object
+const friends={
+    frd_1:"mani",
+    frd_2:"suji"
+}
+// null
+let salary=null;
+// undefined
+let school;
+// array
+let fav_friuts=["mango","banana","blueberry"];
+// data
+let date=new Date();
+console.log("name:"+name+"\n"+"age :"+age+"\n"+ "Maritial Status :"+married+"\n"+"phone no :"+phno+"\n"+
+    "school :"+school +"\n"+"salary :"+salary+"\n"+"favourite Fruites :"+ fav_friuts +"\n"+"Today date :"+date
+
+)
+}
+details()
