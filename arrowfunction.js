6a04d34b107a479b9fa1ddadf9688ca2f816d42f
jeni1402 <paul.jenifer@datasirpi.com>
2026-03-18 10:07:10 +0530

    arrow_function

---
+++ arrowfunction.js
@@ -0,0 +1,17 @@
+arr=["moni",25,"tuty"]
+
+const details=()=>{
+    console.log(...arr)
+
+}
+details()
+
+const add=(x,y)=>x+y
+    console.log("sum :"
+        +add(2,3)
+    )
+
+const getuser=()=>({
+fname :"jeni"
+})
+console.log(getuser())
