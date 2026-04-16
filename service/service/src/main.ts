2f732b5067c67fdf74a65dbc26f5df95a4265235
jeni1402 <paul.jenifer@datasirpi.com>
2026-04-16 16:02:25 +0530

    services in angular

---
+++ service/service/src/main.ts
@@ -0,0 +1,52 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Component, Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class CounterService {
+  value = 0;
+  inc() { this.value++; }
+  dec() { this.value--; }
+}
+
+@Component({
+  selector: 'counter-a',
+  standalone: true,
+  template: `
+    <h4>Counter A</h4>
+    <p>Value: {{ counter.value }}</p>
+    <button (click)="counter.inc()">+1</button>
+    <button (click)="counter.dec()">-1</button>
+  `
+})
+export class CounterA {
+  constructor(public counter: CounterService) {}
+}
+
+@Component({
+  selector: 'counter-b',
+  standalone: true,
+  template: `
+    <h4>Counter B</h4>
+    <p>Value: {{ counter.value }}</p>
+    <button (click)="counter.inc()">+1</button>
+    <button (click)="counter.dec()">-1</button>
+  `
+})
+export class CounterB {
+  constructor(public counter: CounterService) {}
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CounterA, CounterB],
+  template: `
+    <h3>Shared Service Across Components</h3>
+    <counter-a></counter-a>
+    <counter-b></counter-b>
+    <p><em>Both components use the same CounterService instance.</em></p>
+  `
+})
+export class App {}
+
+bootstrapApplication(App);
